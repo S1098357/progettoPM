2633e86bd6ca8790fc229f01f09fc1d3db8add3e
S1098357 <s1098357@studenti.univpm.it>
2023-07-06 13:35:52 +0200

    rimozione proprietà dai preferiti

--- app/src/main/java/com/example/myapplication/PreferitiAdapter.kt
+++ app/src/main/java/com/example/myapplication/PreferitiAdapter.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
 
 class PreferitiAdapter : RecyclerView.Adapter<PreferitiAdapter.PreferitiViewHolder>() {
     private var propertyValues: List<PropertyValue> = emptyList()
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbref: DatabaseReference
+    private lateinit var propertyValueList: ArrayList<PropertyValue>
 
     fun setPropertyValues(propertyValues: List<PropertyValue>) {
         this.propertyValues = propertyValues
@@ -21,16 +31,48 @@ class PreferitiAdapter : RecyclerView.Adapter<PreferitiAdapter.PreferitiViewHold
     ): PreferitiAdapter.PreferitiViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.property_saved, parent, false)
+        propertyValueList= ArrayList()
+        mAuth=FirebaseAuth.getInstance()
+        mDbref= FirebaseDatabase.getInstance("https://unifind-53d53-default-rtdb.europe-west1.firebasedatabase.app/").getReference()
         return PreferitiViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PreferitiAdapter.PreferitiViewHolder, position: Int) {
         val propertyValue = propertyValues[position]
         holder.bind(propertyValue)
-        holder.itemView.setOnClickListener {
+        val cliccabile=holder.itemView.findViewById<LinearLayout>(R.id.cliccabile)
+        cliccabile.setOnClickListener {
             val intent=Intent(it.context,MapsActivity::class.java)
             intent.putExtra("propertyCode",propertyValue.propertyCode)
             it.context.startActivity(intent)
+        }
+        val bottone=holder.itemView.findViewById<ImageButton>(R.id.imageButton4)
+        bottone.setOnClickListener {
+            mDbref.child("property").child("values").addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+
+                    propertyValueList.clear()
+                    var codice: String?
+                    for (postSnapshot in snapshot.children) {
+                        val value = postSnapshot.getValue(PropertyValue::class.java)
+                        codice = value?.propertyCode
+                        if (codice != propertyValue.propertyCode && propertyValue.user==FirebaseAuth.getInstance().currentUser?.uid) {
+                            propertyValueList.add(value!!)
+                        }
+                    }
+                    if (propertyValueList.size!=0) {
+                        mDbref.child("property").child("values").setValue(propertyValueList)
+                        Toast.makeText(it.context,"entra",Toast.LENGTH_LONG).show()
+                    } else {
+                        mDbref.child("property").child("values").setValue(null)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
         }
     }
 
